95e4f328776f73f207d20adafa7323393b33afc4
gautaz <gautaz.github@gmail.com>
2021-05-26 16:34:04 +0200

    to use with postinstall from now

--- index.js
+++ index.js
@@ -1,19 +1,69 @@
-module.exports = (configSelector) => {
-  if (configSelector === 'husky') {
-    return (objectConfig) => ({
-      hooks: {
-        // tag::pre-commit[]
-        'pre-commit': 'pretty-quick --staged && eslint . --fix',
-        // end::pre-commit[]
-        ...objectConfig,
-      },
-    });
-  } else if (configSelector === 'prettier') {
-    return {
-      singleQuote: true,
-      semi: true,
-      tabWidth: 2,
-      printWidth: 120,
-    };
+import { promises as fs } from 'fs';
+import { createRequire } from 'module';
+import path from 'path';
+import husky from 'husky';
+
+// JSON import is still experimental and "--experimental-json-modules" cannot be implied
+const require = createRequire(import.meta.url);
+const packagejson = require('./package.json');
+
+const toJSON = (x) => JSON.stringify(x, null, 2);
+const toMultiline = (array) => array.reduce((acc, current) => `${acc}${current}\n`);
+
+const eslintConfiguration = { extends: ['@r2d2bzh'] };
+const eslintIgnore = ['node_modules'];
+
+const preCommit = [
+  // tag::pre-commit[]
+  'npx --no pretty-quick --staged',
+  'npx --no eslint --fix .',
+  // end::pre-commit[]
+];
+
+const prettierConfiguration = {
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  printWidth: 120,
+};
+const prettierIgnore = ['__fixtures__', 'helm', '*.json', '*.yml', '*.yaml'];
+
+const configurationFiles = {
+  eslint: {
+    '.eslintrc.json': toJSON(eslintConfiguration),
+    '.eslintignore': toMultiline(eslintIgnore),
+  },
+  prettier: {
+    '.prettierrc.json': toJSON(prettierConfiguration),
+    '.prettierignore': toMultiline(prettierIgnore),
+  },
+};
+
+const setConfiguration = (files) =>
+  Promise.all(
+    Object.entries(files).map(async ([toolName, configFiles]) => {
+      await Promise.all(
+        Object.entries(configFiles).map(([name, data]) => fs.writeFile(path.join(process.cwd(), name), data))
+      );
+      console.log(
+        `${packagejson.name}[${packagejson.version}]: ${toolName} configuration deployed on ${process.cwd()}`
+      );
+    })
+  );
+
+const setHusky = () => {
+  husky.install();
+  husky.set('.husky/pre-commit', '');
+  preCommit.forEach((command) => husky.add('.husky/pre-commit', command));
+};
+
+export const install = async () => {
+  try {
+    await setConfiguration(configurationFiles);
+    setHusky();
+    console.log(`${packagejson.name}[${packagejson.version}] successfully deployed`);
+  } catch (e) {
+    console.error(`${packagejson.name}[${packagejson.version}] installation failed: ${e.message ? e.message : e}`);
+    throw e;
   }
 };
--- sample.js
+++
@@ -1,5 +0,0 @@
-// JSON import is still experimental: --experimental-json-modules
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const sample = require('./sample.json');
-console.log(sample);
